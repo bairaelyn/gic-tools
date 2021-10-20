8aa7c88fef968ed750b4ddb8b4bd5984954afd89
R Bailey <rachel.bailey@oeaw.ac.at>
2021-10-20 15:29:59 +0200

    Added read and write functions for measurement files.

--- gictools/meas.py
+++ gictools/meas.py
@@ -6,14 +6,16 @@ gictools.meas
 Tools for handling GIC measurement data and stations.
 
 Created 2015-2021 by R Bailey, ZAMG Conrad Observatory (Vienna).
-Last updated July 2021.
+Last updated October 2021.
 --------------------------------------------------------------------
 """
 
 import os
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
+import numpy as np
+import pandas as pd
 
 #####################################################################
 #                   MeasurementStation Class
@@ -124,6 +126,40 @@ class MeasurementStation:
 #                           FUNCTIONS
 # *******************************************************************
 
+def calc_ab_for_gic_from_E(Ex, Ey, GIC):
+    """Taken from Pulkkinen et al. (2007), EPS, Eqs. (4-5):
+    Determination of ground conductivity and system parameters for optimal modeling of
+    geomagnetically induced current flow in technological systems.
+
+    Returns the parameters a and b for the equation GIC = a*Ex + b*Ey. It is
+    essentially a least-squares fit.
+
+    Parameters:
+    -----------
+    Ex : np.array
+        Northward geoelectric field component.
+    Ey : np.array
+        Eastward geoelectric field component.
+    GIC : np.array
+        Array of GIC measurements for same time range and sampling rate as Ex & Ey.
+
+    Returns:
+    --------
+    (a, b) : (float, float)
+        The components a and b to satisfy the equation GIC = a*Ex + b*Ey.
+    """
+
+    assert len(Ex) == len(GIC), "Array lengths do not match."
+
+    ExEy = np.nanmean(Ex*Ey)
+    ExEx = np.nanmean(Ex*Ex)
+    EyEy = np.nanmean(Ey*Ey)
+    denominator = (ExEy**2. - ExEx * EyEy)
+    a = (np.nanmean(GIC*Ey) * ExEy - np.nanmean(GIC*Ex) * EyEy) / denominator
+    b = (np.nanmean(GIC*Ex) * ExEy - np.nanmean(GIC*Ey) * ExEx) / denominator
+    return (a, b)
+
+
 def list_stations_with_data_on(testdate, json_dir):
     """Returns a list of the GIC measurement stations with data for any given date.
 
@@ -157,8 +193,7 @@ def list_stations_with_data_on(testdate, json_dir):
 
 
 def list_all_measurement_stations(json_dir):
-    """
-    Returns a list of the GIC measurement stations available.
+    """Returns a list of the GIC measurement stations available.
 
     Parameters:
     -----------
@@ -179,3 +214,110 @@ def list_all_measurement_stations(json_dir):
         all_st.append(json_file.strip(".json"))
 
     return all_st
+
+
+def read_gic_file(filepath, skiplines=11):
+    """For reading original .txt-format GIC files from Austria.
+
+    Parameters:
+    -----------
+    filepath :: str
+        Path to file to be read.
+    skiplines :: int (default=11)
+        Header lines to skip when reading raw data file.
+
+    Returns:
+    --------
+    gic_data :: np.ndarray
+        Structured array of GIC data with cols 'time', 'temp', 'dc'.
+    """
+    datestr = os.path.split(filepath)[-1].strip('.txt').split('_')[-1]
+    datestr = datestr[:4]+'-'+datestr[4:6]+'-'+datestr[6:]
+    datafile = open(filepath, 'rb')
+    datastr = datafile.readlines()
+    gic_data = np.genfromtxt(datastr[skiplines:], delimiter=';', usecols=(1,2,3),
+                             dtype={'names':('time', 'temp', 'dc'), 'formats':('<M8[s]', '<f8', '<f8')},
+                             converters={1: lambda x: np.datetime64(datestr+'T'+x.decode().strip(' ')),
+                                         2: lambda x: float(x.decode()),               # temperature in C
+                                         3: lambda x: float(x.decode())/1000.*(-1)},)  # GICs in mA --> to A
+    return gic_data
+
+
+def write_gic_minute_data(trange, data_path, save_path="mindata", st_json_dir="stations", verbose=True):
+    """Writes daily minute data files (.csv) containing all measurements for that day.
+
+    Parameters:
+    -----------
+    trange :: list of datetime.datetime objects
+        Start and end as datetime objects.
+    data_path :: str
+        Path for directory containing GIC data.
+    save_path :: str
+        Path to save new CSV-formatted daily files of minute data.
+    st_json_dir :: str (default="stations")
+        Directory containing all station details for list_all_measurement_stations()
+
+    Returns:
+    --------
+    None
+    """
+
+    all_st = list_all_measurement_stations(st_json_dir)
+
+    # Create path for files if it does not exist:
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
+    # List of days covered by trange:
+    daylist = [trange[0] + timedelta(days=n) for n in range(0, (trange[1]-trange[0]).days)]
+
+    # Loop through all days:
+    for day in daylist:
+        dayf = datetime.strftime(day, "%Y-%m-%d")
+        dayf_ = dayf.replace("-",'')
+        st_codes, st_nums = [], []
+
+        # Check which stations have measurements:
+        for station in all_st:
+            MSt = MeasurementStation(os.path.join(st_json_dir, station+".json"))
+
+            path_to_data = os.path.join(data_path, 'GIC0{}'.format(MSt.clientnum),'GIC0{}_{}.txt').format(MSt.clientnum, dayf_)
+            if os.path.exists(path_to_data):
+                st_codes.append(MSt.code)
+                st_nums.append(MSt.clientnum)
+
+        # Read existing files:
+        if len(st_codes) > 0:
+            if verbose:
+                print("Writing file for {} with {}...".format(dayf, st_codes))
+
+            df = pd.DataFrame(columns=['time']+st_codes)
+
+            # Read in GIC and time data:
+            for code, num in zip(st_codes, st_nums):
+                gic_data = read_gic_file(os.path.join(data_path, 'GIC0{}'.format(num),
+                                                      'GIC0{}_{}.txt').format(num, dayf_))
+
+                if len(gic_data) != 0.:
+                    try:
+                        df[code] = gic_data['dc']
+                        df['time'] = gic_data['time']
+                    except:
+                        if verbose:
+                            print("Problem writing day {} for {}. Probably missing data.".format(dayf, code))
+                        st_codes.remove(code)
+                        st_nums.remove(num)
+
+            # Set time as index and resample to 1 minute:
+            df = df.set_index('time')
+            # Note that this is centered to avoid an offset of 30s wrt measurements:
+            df.index = df.index + pd.Timedelta(30, unit='s')
+            df_min = df.iloc[:-30].resample('1min').median()
+
+            # Save to CSV:
+            df_min.to_csv(os.path.join(save_path, "GICMEAS_{}.csv".format(dayf)))
+
+    if verbose:
+        print("Writing complete.")
+
+    return
